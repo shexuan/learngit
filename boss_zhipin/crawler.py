31f68960320679674bf887a8f0d15e8716be24e1
shexuan <shexuan523392@gmail.com>
2018-03-22 11:45:00 +0800

    wirte some method avoiding baned into a crawler class.

--- boss_zhipin/crawler.py
+++ boss_zhipin/crawler.py
@@ -17,8 +17,61 @@ from lxml import etree
 import sys
 import io
 import csv
+import random
 
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf8')
+
+
+class Crawler(object):
+    '''some static method to avoid baned by sites.
+    '''
+    def __init__(self, url, propxies):
+        self.url = url
+        self.propxies = propxies
+
+    @staticmethod
+    def user_agent():
+        '''return random User-Agent.'''
+        USER_AGENTS = [
+            "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+            "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+            "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+            "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+            "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+            "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+            "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+            "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+            "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+            "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+            "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+            "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+        ]
+        return random.choice(USER_AGENTS)
+
+    @staticmethod
+    def propxy(propxies=None):
+        '''return a propxy ip.'''
+        if not propxies:
+            propxies = self.propxies
+        if isinstance(propxies, list):
+            return random.choice(propxies)
+        else:
+            return propxies
+
+    @staticmethod
+    def get_html(url=None):
+        '''return response as html for xpath to select elements.'''
+        if not url:
+            url = self.url
+        headers = self.user_agent()
+        res = requests.get(url, headers=headers)
+        html = etree.HTML(res.content)
+        return html
+
 
 
 def get_page_url():
@@ -32,7 +85,7 @@ def get_page_url():
 
 def get_res(url):
     '''return the etree.HTML(...) for xpath to extract elements'''
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+    headers = {'User-Agent': user_agent}
     # get all job pages for bioinformatics
     res = requests.get(url, headers=headers)
     if res.status_code != 200:
@@ -105,9 +158,9 @@ def write_to_csv():
             f_csv.writerow(record)
 
 
-url = "https://www.zhipin.com/job_detail/?query=生物信息分析&scity=100010000&industry=1&position=1"
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-res = requests.get(url, headers=headers)
-html = etree.HTML(res.content)
+# url = "https://www.zhipin.com/job_detail/?query=生物信息分析&scity=100010000&industry=1&position=1"
+# headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+# res = requests.get(url, headers=headers)
+# html = etree.HTML(res.content)
 
 write_to_csv()
